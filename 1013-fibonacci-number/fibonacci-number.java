6e0d80f6a29b8f906a718653aee37cea48bf41b6
MohamedAmaan <116302043+MohamedAmaan@users.noreply.github.com>
2025-05-12 10:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (80.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,25 @@
-class Solution {
-    int[] memo = new int[31];
-    public int fib(int n) {
-        if(n<= 1)return n;
-        if(memo[n] != 0) return memo[n];
+import java.util.*;
 
-        memo[n] = fib(n-1) + fib(n-2);
-        return memo[n];
-        
+class Solution {
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Give input: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int result = fib(n);
+        System.out.println("Output: " + result);
     }
 }
