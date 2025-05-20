df483fe3da29b5f46c02a13411caf71e6d2023ee
MohamedAmaan <116302043+MohamedAmaan@users.noreply.github.com>
2025-05-20 08:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (81.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,19 +1,25 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        int count = 0;
-        for(int i =0;i<n;i++){
-            if(nums[i] == target){
-                count ++;
-                break;
-            }else{
-                count =0;
+        // int n = nums.length;
+        // int count = 0;
+        // for(int i =0;i<n;i++){
+        //     if(nums[i] == target){
+        //         count ++;
+        //         break;
+        //     }else{
+        //         count =0;
+        //     }
+        // }
+        // if(count == 1){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        for (int num:nums){
+            if(num==target){
+                return true;
             }
         }
-        if(count == 1){
-            return true;
-        }else{
-            return false;
-        }   
+        return false;
     }
 }
